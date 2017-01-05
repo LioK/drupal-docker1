c5db5a42036fba43cc0801402f263a90a61496f2
LioK <volha.kurylionak@altoros.com>
2017-01-05 20:05:26 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+case $1 in
+
+  install)
+
+    sudo apt-get -qq update
+    sudo apt-get -y install apt-transport-https ca-certificates unzip
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get -qq update
+    sudo apt-get -y install docker-engine
+    sudo service docker start
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    unzip ./code.zip -d .
+    cd code
+    echo "Installation complete. Run 'install.sh start' to start the Drupal cluster."
+
+  create)
+
+    sudo docker-compose up
+
+  start)
+
+    sudo docker-compose start
+
+  stop)
+
+    sudo docker-compose stop    
+
+  destroy)
+
+    sudo docker-compose down --volumes
+
+  *)
+  echo "Usage: ${JOB}_ctl {install|create|start|stop|destroy}" ;;
+
+esac
+exit 0
